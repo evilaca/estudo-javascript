340ef72378e996577a6af97a4474e852357531f7
evilaca <115052003+evilaca@users.noreply.github.com>
2022-10-11 15:34:42 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# estudo-javascript
+ Estudo e Testes JavaScript
---
+++ aula15/ambiente.js
@@ -0,0 +1,13 @@
+let num = [5, 2, 3, 4]
+num[4] = 6
+num.push(7)
+console.log(num)
+console.log(`O vetor tem ${num.length} posições`)
+
+
+num.sort()
+console.log(num)
+num.push(1)
+console.log(num)
+num.sort()
+console.log(num)
